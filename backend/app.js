d27ac230a59495cecd174437d40f0a1fe23d3c9d
Jean-Louis <jeanlouis.mbaka@gmail.com>
2021-06-17 01:48:20 +0100

    Hashes password for security

--- backend/app.js
+++ backend/app.js
@@ -46,9 +46,10 @@ app.post("/register", (req, res) => {
     if (err) throw err;
     if (doc) res.send("User Already Exists");
     if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
       const newUser = new User({
         username: req.body.username,
-        password: req.body.password,
+        password: hashedPassword,
       });
       await newUser.save();
     }
